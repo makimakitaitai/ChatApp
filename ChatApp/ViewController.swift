1377fd35dc712fdbd0acc5b632aae17840ec8e52
makimakitaitai <s19g470@stu.kagawa-u.ac.jp>
2020-02-12 09:32:01 +0900

    Initial Commit

---
+++ ChatApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by 萬木大志 on 2020/02/12.
+//  Copyright © 2020 makimaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
